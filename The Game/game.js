64954111714b6d2c6ccf166da28466c0391bccd4
Unknown <99053654@mydavinci.nl>
2021-01-07 15:04:30 +0100

    changed it a bit

--- The Game/game.js
+++ The Game/game.js
@@ -1,1 +1,44 @@
-alert('hoi')
+var text1 = 'het is laat in de avond en je gaat een rondje wandelen met je hond op een gegeven moment kom je langs een huis en de deur staat open wat doe je? Typ naar binnen of doorlopen';
+var text2 = '';
+var text3 = '';
+var text4 = '';
+var text5 = '';
+var text6 = '';
+var text7 = '';
+prompt(text1);
+if (text1 == 'naar binnen'){
+	prompt(text2)
+} else {
+	prompt(text3);
+}
+	if (text3 = "1"){
+		prompt(text4);
+	}	else {
+		prompt(text5);
+				}
+		if (text4 = "1"){
+			alert("to be continued")
+		}	else {
+			alert("to be continued")
+			}
+			if (text5 = "1"){
+				alert("to be continued")
+			}	else {
+				alert("to be continued")
+				}
+				if (text2 = "1"){
+					prompt(text6);
+				}	else {
+					prompt(text7);
+					}
+					if (text7 = "1"){
+						alert("to be continued");
+					}	else {
+						alert("to be continued");
+						}
+							
+						if (text6 = "1"){
+							alert("to be continued");
+						}	else {
+							alert("to ben continued");
+							}

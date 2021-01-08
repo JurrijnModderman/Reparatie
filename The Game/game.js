707af75a0d24621f6084da411fb09c34d00e6069
Unknown <99053654@mydavinci.nl>
2021-01-08 12:11:59 +0100

    added the text and changed the code in the right order

--- The Game/game.js
+++ The Game/game.js
@@ -1,44 +1,38 @@
-var text1 = 'het is laat in de avond en je gaat een rondje wandelen met je hond op een gegeven moment kom je langs een huis en de deur staat open wat doe je? Typ naar binnen of doorlopen';
-var text2 = '';
-var text3 = '';
-var text4 = '';
-var text5 = '';
-var text6 = '';
-var text7 = '';
-prompt(text1);
-if (text1 == 'naar binnen'){
-	prompt(text2);
-} else {
-	prompt(text3);
-}
-	if (text3 = "1"){
-		prompt(text4);
+var text1 = prompt('je staat in een kamer met een gesloten deur voor je, om de schat te vinden moet je daar door heen. Je gaat de sleutel proberen te vinden, waar ga je kijken achter het schilderij of onder de bloempot? Typ schilderij of bloempot.');
+if (text1 == 'schilderij'){
+	var text2 = prompt('helaas de sleutel zat niet achter het schilderij, maar omdat dit de eerste deur was mag je de geheime deur achter het behang nemen. achter de deur zit nog een kamer, in die kamer staan 2 potten een rode en een blauwe. welke gooi je kapot? typ rood of blauw');
+	if (text2 == "rood"){
+		var text6 = prompt('je hebt de rode pot kapotgegooid en er liggen een hoop scherven op de grond, maar tussen die scherven ligt een sleutel. dat is de sleutel van de kluis waar de schat in zit verstopt. toch moet je eerst door de volgende deur heen. Je ziet een luciferdoosje en een hamer liggen wat pak je. typ luciferdoosje of hamer');
 	}	else {
-		prompt(text5);
-				}
-		if (text4 = "1"){
-			alert("to be continued");
+		var text7 = prompt('je hebt de blauwe pot kapotgegooid helaas mis je nu iets wat je later nodig hebt maar daar kom je zo wel achter. Je ziet een luciferdoosje en een hamer liggen wat pak je. typ luciferdoosje of hamer');
+		}
+		if (text7 == "luciferdoosje"){
+			alert("Je hebt gekozen voor het luciferdoosje daarmee heb je geprobeerd de deur open te branden, helaas is het hele huis en de schat verbrandt. Geen schat voor jou!");
 		}	else {
-			alert("to be continued");
+			alert("Je hebt gekozen voor de hamer en het is je gelukt de deur open te breken, achter de deur ligt de schat. Om die te openen had je alleen nog een sleutel nodig, en die heb je niet gevonden. Jammer!");
 			}
-			if (text5 = "1"){
-				alert("to be continued");
-			}	else {
-				alert("to be continued");
-				}
-				if (text2 = "1"){
-					prompt(text6);
-				}	else {
-					prompt(text7);
-					}
-					if (text7 = "1"){
-						alert("to be continued");
-					}	else {
-						alert("to be continued");
-						}
 							
-						if (text6 = "1"){
-							alert("to be continued");
-						}	else {
-							alert("to ben continued");
-							}
+			if (text6 == "luciferdoosje"){
+				alert("Je hebt gekozen voor het luciferdoosje daarmee heb je geprobeerd de deur open te branden, helaas is het hele huis en de schat verbrandt. Geen schat voor jou!");
+			}	else {
+				alert("Je hebt gekozen voor de hamer en het is je gelukt de deur open te breken, achter de deur ligt de schat. Om die te openen had je alleen nog een sleutel nodig, gelukkig had je die nog op zak. Het is je gelukt, Gefeliciteerd!");
+				}
+} else {
+	var text3 = prompt('goed geraden de sleutel lag inderdaad onder de bloempot, je mag de deur door. Achter de deur zit nog een kamer, in die kamer staan 2 potten een rode en een blauwe. welke gooi je kapot? typ rood of blauw');
+}
+	if (text3 == "rood"){
+		var text4 = prompt('je hebt de rode pot kapotgegooid en er liggen een hoop scherven op de grond, maar tussen die scherven liggen twee sleutels. de eerste is de sleutel van de kluis waar de schat in zit verstopt, de tweede is een geheim. toch moet je eerst door de volgende deur heen. Je ziet een luciferdoosje en een hamer liggen wat pak je. typ luciferdoosje of hamer');
+	}	else {
+		var text5 = prompt('je hebt de blauwe pot kapotgegooid helaas mis je nu iets wat je later nodig hebt maar daar kom je zo wel achter. Je ziet een luciferdoosje en een hamer liggen wat pak je. typ luciferdoosje of hamer');
+		}
+		if (text4 == "luciferdoosje"){
+			alert("Je hebt gekozen voor het luciferdoosje daarmee heb je geprobeerd de deur open te branden, helaas is het hele huis en de schat verbrandt. Geen schat voor jou!");
+		}	else {
+			alert("Je hebt gekozen voor de hamer en het is je gelukt de deur open te breken, achter de deur ligt de schat. Om die te openen had je alleen nog een sleutel nodig, gelukkig had je die nog op zak. Ook had je nog een tweede sleutel gevonden, dat is voor iets in de schat. Het is je gelukt, Gefeliciteerd!");
+			}
+			if (text5 == "luciferdoosje"){
+				alert("Je hebt gekozen voor het luciferdoosje daarmee heb je geprobeerd de deur open te branden, helaas is het hele huis en de schat verbrandt. Geen schat voor jou!");
+			}	else {
+				alert("Je hebt gekozen voor de hamer en het is je gelukt de deur open te breken, achter de deur ligt de schat. Om die te openen had je alleen nog een sleutel nodig, gelukkig had je die nog op zak. Het is je gelukt, Gefeliciteerd!");
+				}
+				

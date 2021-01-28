d43a8138fef67fc2b2e920196d50b1ed963a6b8b
Unknown <99053654@mydavinci.nl>
2021-01-28 15:16:24 +0100

    changed

--- adventure game/lab buttons/buttons.js
+++ adventure game/lab buttons/buttons.js
@@ -1,7 +1,18 @@
-const keuze1 = document.getElementById('1');
+/*const keuze1 = document.getElementById('1');
 
 function button1() {
 	keuze1.src="images/1.jpg";
-}
+}*/
 
+function button1(){ 
+    document.getElementById('1').backgroundimage = url('images/1.jpg'; 
+} 
+      
+function button2(){ 
+    document.getElementById('2').style.backgroundImage = "images/2.jpg"; 
+} 
+
+function button3() {
+    document.getElementById('3').style.backgroundImage = 'images/3.jpg';
+}
 //body.style.backgroundimage = src="";

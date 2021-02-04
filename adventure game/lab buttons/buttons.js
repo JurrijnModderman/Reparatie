3054f745e80502b0b464c0e8542021c44414262f
Unknown <99053654@mydavinci.nl>
2021-02-04 15:13:30 +0100

    counter works

--- adventure game/lab buttons/buttons.js
+++ adventure game/lab buttons/buttons.js
@@ -16,14 +16,43 @@ function button3() {
     document.getElementById('3').style.backgroundImage = 'url(images/3.jpg)';
 }*/
 //body.style.backgroundimage = src="";
+var button1 = 0;
+var button2 = 0;
+var button3 = 0;
+
 function changeBG() {
 	document.body.style.backgroundImage = "url('images/1.jpg')";
+	button1++;
+	console.log('value button 1 = ' + button1);
 }
+
+/*if (document.getElementById('button1').onclick == true) {
+	button1++;
+	console.log(button1);
+}*/
+/*document.getElementById('button1').onclick = function() {
+	button1++;
+	console.log('value button 1 = ' + button1);
+}*/
 
 function changeBG2() {
 	document.body.style.backgroundImage = "url('images/2.jpg')";
+	button2++;
+	console.log('value button 2 = ' + button2);
 }
+
+/*document.getElementById('button2').onclick = function() {
+	button2++;
+	console.log('value button 2 = ' + button2);
+}*/
 
 function changeBG3() {
 	document.body.style.backgroundImage = "url('images/3.jpg')";
+	button3++;
+	console.log('value button 3 = ' + button3);
 }
+
+/*document.getElementById('button3').onclick = function() {
+	button3++;
+	console.log('value button 3 = ' + button3);
+}*/

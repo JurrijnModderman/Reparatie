1bcd9113a23a105e0aaee477125040bdee3ddd87
Unknown <99053654@mydavinci.nl>
2021-02-04 11:40:25 +0100

    buttons are working now

--- adventure game/lab buttons/buttons.js
+++ adventure game/lab buttons/buttons.js
@@ -4,15 +4,26 @@ function button1() {
 	keuze1.src="images/1.jpg";
 }*/
 
-function button1(){ 
-    document.getElementById('1').backgroundimage = url('images/1.jpg'; 
+/*function button1(){ 
+    document.getElementById('1').style.backgroundimage = 'url(images/1.jpg)'; 
 } 
       
 function button2(){ 
-    document.getElementById('2').style.backgroundImage = "images/2.jpg"; 
+    document.getElementById('2').style.backgroundImage = "url(images/2.jpg)"; 
 } 
 
 function button3() {
-    document.getElementById('3').style.backgroundImage = 'images/3.jpg';
-}
+    document.getElementById('3').style.backgroundImage = 'url(images/3.jpg)';
+}*/
 //body.style.backgroundimage = src="";
+function changeBG() {
+	document.body.style.backgroundImage = "url('images/1.jpg')";
+}
+
+function changeBG2() {
+	document.body.style.backgroundImage = "url('images/2.jpg')";
+}
+
+function changeBG3() {
+	document.body.style.backgroundImage = "url('images/3.jpg')";
+}

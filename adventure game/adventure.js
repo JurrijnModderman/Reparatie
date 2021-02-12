25beb22ac7f3adcbe1facce87f951410cedf8b37
Unknown <99053654@mydavinci.nl>
2021-02-12 10:45:05 +0100

    2 buttons are hidden and need to make a story

--- adventure game/adventure.js
+++ adventure game/adventure.js
@@ -1,5 +1,14 @@
 const buttons = document.getElementsByClassName('button');
 
+buttons[1].style.display = 'none';
+buttons[2].style.display = 'none';
+start();
+
+function start() {
+	level1();
+}
+
 function level1(index) {
-	
+	buttons[1].style.display = 'visible';
+	buttons[2].style.display = 'visible';
 }

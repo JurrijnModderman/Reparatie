bd0282ce134ac5afb1fbc450bea71c17909404d8
Unknown <99053654@mydavinci.nl>
2021-02-15 14:49:31 +0100

    added a line for the image

--- adventure game/adventure.js
+++ adventure game/adventure.js
@@ -2,13 +2,14 @@ const buttons = document.getElementsByClassName('button');
 
 buttons[1].style.display = 'none';
 buttons[2].style.display = 'none';
-start();
+// start();
 
-function start() {
-	level1();
-}
+// function start() {
+// 	level1();
+// }
 
 function level1(index) {
 	buttons[1].style.display = 'visible';
 	buttons[2].style.display = 'visible';
+	document.body.style.backgroundImage = '';
 }

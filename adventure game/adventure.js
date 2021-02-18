ae19033c14c80b1cf57364c5558741f65744a7a5
Unknown <99053654@mydavinci.nl>
2021-02-18 14:36:04 +0100

    changed some things with variables, feedback: need to make arrays where i can get out the content and need to create less functions cuz there all almost the same

--- adventure game/adventure.js
+++ adventure game/adventure.js
@@ -1,22 +1,30 @@
 // const buttons = document.getElementsByClassName('button');
-button1 = document.getElementById('button1');
-button2 = document.getElementById('button2');
-button3 = document.getElementById('button3');
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+const button3 = document.getElementById('button3');
 const container = document.getElementById('game-container');
 const title = document.getElementById('title');
 const description = document.getElementById('description');
 
 // buttons[1].style.display = 'none';
 // buttons[2].style.display = 'none';
+// tekst uit array ophalen
+//meegekregen paramter van function
+// var map images, array
+const imageMap = 'Images';
 
 Start();
 
 function Start() {
 	title.innerHTML = 'Wie is het';
 	description.innerHTML = 'Wie is het? Je krijgt een afbeelding te zien en dan moet je raden wie het is!';
+	button1.innerHTML = 'Start';
 	button2.style.display = 'none';
 	button3.style.display = 'none';
-	container.style.backgroundImage = "url('Images/Wie_moet_ik_hebben.jpg')";
+	// container.style.backgroundImage = "url('Images/Wie_moet_ik_hebben.jpg')";
+	container.style.backgroundImage = 'url(' + imageMap + '/Wie_moet_ik_hebben.jpg)';
+	//'url(' + imageMap + images[0] + ')';
+	//Staged level 1, zodat die pas wordt uitgevoerd als je op de button klikt
 	button1.onclick = level1;
 }
 
@@ -25,13 +33,11 @@ function level1() {
 	description.innerHTML = 'Is dit Ronald Koeman?';
 	button1.style.display = 'block';
 	button2.style.display = 'block';
-	button3.style.display = 'none';
+	// button3.style.display = 'none';
 	container.style.backgroundImage = "url('Images/Koeman.jpg')";
 	button1.innerHTML = 'ja';
 	button2.innerHTML = 'nee';
 	button1.onclick = level2;
-	// button2.onclick = Start;
-	// button2.innerHTML = 'Start';
 }
 
 function level2() {
@@ -107,7 +113,7 @@ function level10() {
 
 function einde() {
 	title.innerHTML = 'Einde';
-	description.innerHTML = 'Je hebt gewonnen!!!';
+	description.innerHTML = 'Je hebt gewonnen!!!' + '<br>' + 'Wil je nog een keer spelen? Klik dan op Start!';
 	container.style.backgroundImage = "url('Images/Gewonnen.jpg')";
 	button1.innerHTML = 'Start';
 	button2.style.display = 'none';

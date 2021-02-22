70d60007355783d0b3fb58d48fff396e88cc4793
Unknown <99053654@mydavinci.nl>
2021-02-22 11:58:58 +0100

    changed

--- adventure game/adventure.js
+++ adventure game/adventure.js
@@ -7,9 +7,13 @@ const title = document.getElementById('title');
 const description = document.getElementById('description');
 const imageMap = 'url(' + 'Images' + '/';
 
-const scenes = [
+const beginEnd = [
 	{title: "Wie is het", description: "Wie is het? Je krijgt een afbeelding te zien en dan moet je raden wie het is!", image: imageMap + 'Wie_moet_ik_hebben.jpg)'}
-	,{title: "Vraag 1", description: "Is dit Ronald Koeman?", image: imageMap + 'Koeman.jpg)', answer: 'ja'}
+	,{title: "Einde", description: "Je hebt gewonnen!!!" + '<br>' + 'Wil je nog een keer spelen? Klik dan op Start!', image: imageMap + 'Gewonnen.jpg)'}
+];
+const scenes = [
+	//{title: "Wie is het", description: "Wie is het? Je krijgt een afbeelding te zien en dan moet je raden wie het is!", image: imageMap + 'Wie_moet_ik_hebben.jpg)'}
+	{title: "Vraag 1", description: "Is dit Ronald Koeman?", image: imageMap + 'Koeman.jpg)', answer: 'ja'}
 	,{title: "Vraag 2", description: "Is dit Frank Rijkaard?", image: imageMap + 'Frank-Rijkaard.jpg)', answer: 'ja'}
 	,{title: "Vraag 3", description: "Is dit Edwin van der Sar?", image: imageMap + 'Frank-de-Boer.jpg)', answer: 'nee'}
 	,{title: "Vraag 4", description: "Is dit Edgar-Davids?", image: imageMap + 'Edgar-Davids.jpg)', answer: 'ja'}
@@ -19,7 +23,7 @@ const scenes = [
 	,{title: "Vraag 8", description: "Is dit Marc Overmars?", image: imageMap + 'Edwin-van-der-Sar.jpg)', answer: 'nee'}
 	,{title: "Vraag 9", description: "Is dit Ruud van Nistelrooy?", image: imageMap + 'Marco-van-Basten.jpg)', answer: 'nee'}
 	,{title: "Vraag 10", description: "Is dit Ruud Gullit?", image: imageMap + 'Ruud-Gullit.jpg)', answer: 'ja'}
-	,{title: "Einde", description: "Je hebt gewonnen!!!" + '<br>' + 'Wil je nog een keer spelen? Klik dan op Start!', image: imageMap + 'Gewonnen.jpg)'}
+	//,{title: "Einde", description: "Je hebt gewonnen!!!" + '<br>' + 'Wil je nog een keer spelen? Klik dan op Start!', image: imageMap + 'Gewonnen.jpg)'}
 ];
 
 
@@ -32,15 +36,16 @@ const scenes = [
 var sceneIndex = 0;
 start();
 console.log(scenes);
+console.log(beginEnd);
 
 function start() {
-	title.innerHTML = scenes[0].title;
-	description.innerHTML = scenes[0].description;
-	container.style.backgroundImage = scenes[0].image;
+	title.innerHTML = beginEnd[0].title;
+	description.innerHTML = beginEnd[0].description;
+	container.style.backgroundImage = beginEnd[0].image;
 	button1.innerHTML = 'Start';
 	button2.style.display = 'none';
 	button3.style.display = 'none';
-	sceneIndex = 1;
+	// sceneIndex = 1;
 	button1.onclick = loadScene;
 	// container.style.backgroundImage = "url('Images/Wie_moet_ik_hebben.jpg')";
 	//'url(' + imageMap + images[0] + ')';
@@ -52,16 +57,19 @@ function Choice(choice) {
 		console.log("correct");
 		console.log(sceneIndex);
 		sceneIndex++;
+		console.log(sceneIndex);
 		if (sceneIndex == 10) {
 		console.log(sceneIndex);
 		einde();
 		}
+		loadScene();
 	} else {
 		console.log("incorrect");
 		console.log(sceneIndex);
+		start;
 		sceneIndex = 0;
+		console.log(sceneIndex);
 	}
-	loadScene();
 }
 
 function loadScene() {
@@ -74,12 +82,13 @@ function loadScene() {
 }
 
 function einde() {
-	title.innerHTML = scenes[11].title;
-	description.innerHTML = scenes[11].description;
-	container.style.backgroundImage = scenes[11].image;
+	title.innerHTML = beginEnd[1].title;
+	description.innerHTML = beginEnd[1].description;
+	container.style.backgroundImage = beginEnd[1].image;
 	button2.style.display = 'none';
 	button3.style.display = 'none';
 	button1.style.display = 'block';
+	button1.innerHTML = 'Start';
 	button1.onclick = start;
 }
 

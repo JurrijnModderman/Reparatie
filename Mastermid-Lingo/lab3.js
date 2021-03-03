643c34aa2a1df06586438e7f5f13f85cee8a6237
Unknown <99053654@mydavinci.nl>
2021-03-03 13:07:07 +0100

    added the css, a couple simple functions and only need to do the css now

--- Mastermid-Lingo/lab3.js
+++ Mastermid-Lingo/lab3.js
@@ -1,3 +1,24 @@
-var button1 = document.createElement("button");
-var button2 = document.createElement("button");
-var button3 = document.createElement("button");
+var button1 = document.createElement("BUTTON");
+button1.innerHTML = "Button1";
+document.getElementById("container").appendChild(button1);
+var button2 = document.createElement("BUTTON");
+button2.innerHTML = "Button2";
+document.getElementById("container").appendChild(button2);
+var button3 = document.createElement("BUTTON");
+button3.innerHTML = "Button3";
+document.getElementById("container").appendChild(button3);
+
+
+button1.onclick = groen;
+button2.onclick = rood;
+button3.onclick = blauw;
+
+function groen() {
+	document.body.style.backgroundColor = 'green';
+}
+function rood() {
+	document.body.style.backgroundColor = 'red';
+}
+function blauw() {
+	document.body.style.backgroundColor = 'blue';
+}

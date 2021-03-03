c0362d1b89c64bb31ddc07a5a1080d90873f33f6
Unknown <99053654@mydavinci.nl>
2021-03-03 13:25:06 +0100

    color is white now

--- Mastermid-Lingo/lab3.js
+++ Mastermid-Lingo/lab3.js
@@ -1,14 +1,17 @@
 var button1 = document.createElement("BUTTON");
 button1.innerHTML = "Button1";
 button1.style.backgroundColor = 'green';
+button1.style.color = 'white';
 document.getElementById("container").appendChild(button1);
 var button2 = document.createElement("BUTTON");
 button2.innerHTML = "Button2";
 button2.style.backgroundColor = 'red';
+button2.style.color = 'white';
 document.getElementById("container").appendChild(button2);
 var button3 = document.createElement("BUTTON");
 button3.innerHTML = "Button3";
 button3.style.backgroundColor = 'blue';
+button3.style.color = 'white';
 document.getElementById("container").appendChild(button3);
 
 

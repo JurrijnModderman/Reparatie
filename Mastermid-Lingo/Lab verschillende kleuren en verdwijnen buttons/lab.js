10677e986fb43d1a66e39bf33318b742dcf6c0e5
Unknown <99053654@mydavinci.nl>
2021-03-05 11:55:32 +0100

    getting errors code doesnt work

--- Mastermid-Lingo/Lab verschillende kleuren en verdwijnen buttons/lab.js
+++ Mastermid-Lingo/Lab verschillende kleuren en verdwijnen buttons/lab.js
@@ -1,5 +1,6 @@
 const container = document.getElementById("container");
 var buttons = [];
+var clickIndex = 1;
 //let locks the number for that loop
 for (let nummer = 0; nummer <= 29; nummer++) {
 	var button = document.createElement("BUTTON");
@@ -8,11 +9,26 @@ for (let nummer = 0; nummer <= 29; nummer++) {
 	buttons[nummer].innerHTML = nummer + 1;
 	buttons[nummer].style.backgroundColor = 'green';
 	container.appendChild(button);
-	button.addEventListener("click", function(){red(nummer)});
+	button.addEventListener("click", function(){colors(nummer)});
 }
 
-function red(index) {
+function colors(index) {
+	console.log(clickIndex);
 	console.log(index);
 	console.log(buttons);
-	buttons[index].style.backgroundColor = 'red';
+	if (clickIndex = 1) {
+		buttons[index].style.backgroundColor = 'red';	}
+	if (clickIndex = 2) {
+		buttons[index].style.backgroundColor = 'purple';
+	}
+	if (clickIndex = 3) {
+		buttons[index].style.backgroundColor = 'blue';
+	}
+	if (clickIndex = 4) {
+		buttons[index].style.backgroundColor = 'black';
+	}
+	clickIndex++;
+	if (clickIndex = 5) {
+		container.removeChild(buttons[index]);
+	}
 }

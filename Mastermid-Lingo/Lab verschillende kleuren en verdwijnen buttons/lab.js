e4298cdbf51e1c80ab014c7a9b3960fb66cad8c4
Unknown <99053654@mydavinci.nl>
2021-03-08 12:57:00 +0100

    Got help from Stijn and the lab is working the correct way now

--- Mastermid-Lingo/Lab verschillende kleuren en verdwijnen buttons/lab.js
+++ Mastermid-Lingo/Lab verschillende kleuren en verdwijnen buttons/lab.js
@@ -1,34 +1,37 @@
 const container = document.getElementById("container");
 var buttons = [];
-var clickIndex = 1;
+var clickAmount = [];
 //let locks the number for that loop
 for (let nummer = 0; nummer <= 29; nummer++) {
 	var button = document.createElement("BUTTON");
 	button.classList.add("box");
 	buttons.push(button);
+	clickAmount.push(0);
 	buttons[nummer].innerHTML = nummer + 1;
 	buttons[nummer].style.backgroundColor = 'green';
 	container.appendChild(button);
+	//meer dynamisch(addeventlistener)
 	button.addEventListener("click", function(){colors(nummer)});
 }
 
 function colors(index) {
-	console.log(clickIndex);
+	console.log(clickAmount);
 	console.log(index);
 	console.log(buttons);
-	if (clickIndex == 1) {
-		buttons[index].style.backgroundColor = 'red';	}
-	if (clickIndex == 2) {
+	clickAmount[index]++;
+	if (clickAmount[index] == 1) {
+		buttons[index].style.backgroundColor = 'red';	
+	}
+	if (clickAmount[index] == 2) {
 		buttons[index].style.backgroundColor = 'purple';
 	}
-	if (clickIndex == 3) {
+	if (clickAmount[index] == 3) {
 		buttons[index].style.backgroundColor = 'blue';
 	}
-	if (clickIndex == 4) {
+	if (clickAmount[index] == 4) {
 		buttons[index].style.backgroundColor = 'black';
 	}
-	if (clickIndex == 5) {
+	if (clickAmount[index] == 5) {
 		container.removeChild(buttons[index]);
 	}
-	buttons[index].clickIndex++;
 }

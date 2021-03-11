fcb6b2abf376690908737519c7e9a9ec7f588961
Unknown <99053654@mydavinci.nl>
2021-03-11 15:06:09 +0100

    added a check for the input(word)

--- Mastermid-Lingo/Lab Speelbord/speelbord.js
+++ Mastermid-Lingo/Lab Speelbord/speelbord.js
@@ -13,24 +13,28 @@ function check() {
 	var word = wordInput.value.toLowerCase();
 	var letter = letterInput.value.toLowerCase();
 	console.log(letter);
-	if (word.includes(letter) == true) {
-		if (word.charAt(0) == letter){
-			item[0].innerHTML = letter;
-		}  
-		if (word.charAt(1) == letter) {
-			item[1].innerHTML = letter;
-		}  
-		if (word.charAt(2) == letter) {
-			item[2].innerHTML = letter;
-		}  
-		if (word.charAt(3) == letter) {
-			item[3].innerHTML = letter;
-		}  
-		if (word.charAt(4) == letter) {
-			item[4].innerHTML = letter;
+	if (word.length == 5) {
+		if (word.includes(letter) == true) {
+			if (word.charAt(0) == letter){
+				item[0].innerHTML = letter;
+			}  
+			if (word.charAt(1) == letter) {
+				item[1].innerHTML = letter;
+			}  
+			if (word.charAt(2) == letter) {
+				item[2].innerHTML = letter;
+			}  
+			if (word.charAt(3) == letter) {
+				item[3].innerHTML = letter;
+			}  
+			if (word.charAt(4) == letter) {
+				item[4].innerHTML = letter;
+			}
+			// item[0].innerHTML = letter;
+		} else {
+			alert('This letter is not included in your chosen word!');
 		}
-		// item[0].innerHTML = letter;
 	} else {
-		alert('This letter is not included in your chosen word!');
+		alert('Input need to be a 5 letter word!');
 	}
 }

27f44f7d83a7b3f5ae540057519a137d30924956
Unknown <99053654@mydavinci.nl>
2021-03-11 10:54:25 +0100

    deleted the form(dont need it) changed the css, it checks now if the inputletter is in the inputword

--- Mastermid-Lingo/Lab Speelbord/speelbord.js
+++ Mastermid-Lingo/Lab Speelbord/speelbord.js
@@ -1,9 +1,17 @@
-const word = document.getElementById("Word");
-const letter = document.getElementById("Letter");
+const wordInput = document.getElementById("Word");
+const letterInput = document.getElementById("Letter");
 const item = document.getElementsByClassName("grid-item");
-const form = document.getElementById("myForm");
-
+/*const form = document.getElementById("myForm");*/
+//volgorde opschrijven letter uit woord halen en dan op de juiste positie neerzetten
 function check() {
+	console.log(letterInput);
+	console.log(wordInput);
+	var word = wordInput.value;
+	var letter = letterInput.value;
 	console.log(letter);
-	item[0].innerHTML = letter.value;
+	if (word.includes(letter) == true) {
+		item[0].innerHTML = letter;
+	} else {
+		alert('This letter is not included in your chosen word!');
+	}
 }

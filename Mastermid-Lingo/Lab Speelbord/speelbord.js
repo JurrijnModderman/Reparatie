3dd18aefa923d1dbc3312554a996c4c5d7ff8ac3
Unknown <99053654@mydavinci.nl>
2021-03-12 09:42:10 +0100

    if letter == letter backgroundcolor = green

--- Mastermid-Lingo/Lab Speelbord/speelbord.js
+++ Mastermid-Lingo/Lab Speelbord/speelbord.js
@@ -14,27 +14,48 @@ function check() {
 	var letter = letterInput.value.toLowerCase();
 	console.log(letter);
 	if (word.length == 5) {
-		if (word.includes(letter) == true) {
-			if (word.charAt(0) == letter){
-				item[0].innerHTML = letter;
-			}  
-			if (word.charAt(1) == letter) {
-				item[1].innerHTML = letter;
-			}  
-			if (word.charAt(2) == letter) {
-				item[2].innerHTML = letter;
-			}  
-			if (word.charAt(3) == letter) {
-				item[3].innerHTML = letter;
-			}  
-			if (word.charAt(4) == letter) {
-				item[4].innerHTML = letter;
-			}
-			// item[0].innerHTML = letter;
-		} else {
-			alert('This letter is not included in your chosen word!');
+		if (word.charAt(0) == letter.charAt(0)) {
+			item[0].innerHTML = letter.charAt(0);
+			item[0].style.backgroundColor = 'green';
+		}
+		if (word.charAt(1) == letter.charAt(1)) {
+			item[1].innerHTML = letter.charAt(1);
+			item[1].style.backgroundColor = 'green';
+		}
+		if (word.charAt(2) == letter.charAt(2)) {
+			item[2].innerHTML = letter.charAt(2);
+			item[2].style.backgroundColor = 'green';
+		}
+		if (word.charAt(3) == letter.charAt(3)) {
+			item[3].innerHTML = letter.charAt(3);
+			item[3].style.backgroundColor = 'green';
+		}
+		if (word.charAt(4) == letter.charAt(4)) {
+			item[4].innerHTML = letter.charAt(4);
+			item[4].style.backgroundColor = 'green';
 		}
 	} else {
 		alert('Input need to be a 5 letter word!');
 	}
 }
+//lab speelbord(opdracht 2);
+// if (word.includes(letter) == true) {
+		// 	if (word.charAt(0) == letter){
+		// 		item[0].innerHTML = letter;
+		// 	}  
+		// 	if (word.charAt(1) == letter) {
+		// 		item[1].innerHTML = letter;
+		// 	}  
+		// 	if (word.charAt(2) == letter) {
+		// 		item[2].innerHTML = letter;
+		// 	}  
+		// 	if (word.charAt(3) == letter) {
+		// 		item[3].innerHTML = letter;
+		// 	}  
+		// 	if (word.charAt(4) == letter) {
+		// 		item[4].innerHTML = letter;
+		// 	}
+		// 	// item[0].innerHTML = letter;
+		// } else {
+		// 	alert('This letter is not included in your chosen word!');
+		// }

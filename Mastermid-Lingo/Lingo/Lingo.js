dca12838849704416ec08eee5829dad1fca46abd
Unknown <99053654@mydavinci.nl>
2021-03-16 11:17:00 +0100

    added the file for the words and copied some code from the lab that needs to be exactly the same

---
+++ Mastermid-Lingo/Lingo/Lingo.js
@@ -0,0 +1,28 @@
+const wordInput = document.getElementById("Word");
+const letterInput = document.getElementById("Letter");
+const item = document.getElementsByClassName("grid-item");
+
+function check() {
+	console.log(letterInput);
+	console.log(wordInput);
+	var word = wordInput.value.toLowerCase();
+	var letter = letterInput.value.toLowerCase();
+	console.log(letter);
+	if (word.length == 5) {
+		for (index = 0; index <= 4; index++) {
+			item[index].innerHTML = letter.charAt(index);
+			//for loop om de letters van het ingevoerde woord te vergelijken met het te raden woord
+			for (letterIndex = 0; letterIndex <= 4; letterIndex++) {
+				if (letter.charAt(index) == word.charAt(letterIndex)) {
+					if (word.charAt(index) == letter.charAt(index)) {
+		 				item[index].style.backgroundColor = 'green';
+		 			} else {
+		 				item[index].style.backgroundColor = 'yellow';
+		 			}
+				}
+			}
+		}
+	} else {
+		alert('Input need to be a 5 letter word!');
+	}
+}

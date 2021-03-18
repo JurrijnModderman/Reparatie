4b8afb6412900b29016be9a1754728cd63818065
Unknown <99053654@mydavinci.nl>
2021-03-18 12:18:59 +0100

    added a parameter and classes to hide and show elements

--- Mastermid-Lingo/Lingo/Lingo.js
+++ Mastermid-Lingo/Lingo/Lingo.js
@@ -15,17 +15,16 @@ button.onclick = start;
 start();
 //start functie om de hoofdpagina te creeeren
 function start() {
-	empty();
+	empty(1);
 	beginText.innerHTML = 'Welcome to Lingo!' + '<br>' + 'You get the first letter of a 5 letter word that you need to guess! You have 5 guesses. If your answer is right, you will get another word! Else, you come back on this page!' + '<br>' + 'Good Luck!!!';
-	letter.classList.add("letterStyle");
-	grid.classList.add("gridStyle");
+	console.log('gridStyle');
 	//letter.style.display = 'none';
 	//grid.style.display = 'none';
 	button.value = 'Start';
 	button.onclick = loadScene;
 }
 //empty functie om alle 'gebruikte' elementen weer leeg te gooien
-function empty() {
+function empty(hide) {
 	for (gridItem = 0; gridItem <=4; gridItem++) {
 		item[gridItem].innerHTML = gridItem+1;
 		item[gridItem].style.backgroundColor = 'white';
@@ -35,15 +34,24 @@ function empty() {
 	randomWordId.innerHTML = '';
 	yourGuess.innerHTML = '';
 	letter.value = '';
+	if (hide != 2) {
+		letter.classList.remove("showinput");
+		letter.classList.add("hideinput");
+		grid.classList.remove("showgrid");
+		grid.classList.add("hidegrid");
+	} else {
+		letter.classList.remove("hideinput");
+		letter.classList.add("showinput");
+		grid.classList.remove("hidegrid");
+		grid.classList.add("showgrid");
+	}
 }
 //loadScene functie om een randomword te genereren en daarvan de eerste letter te tonen op het scherm en de rest van die pagina te creeeren
 function loadScene() {
-	empty();
+	empty(2);
 	var randomWordIndex = Math.floor(Math.random() * words.length);
 	randomWord = words[randomWordIndex];
 	console.log(randomWord);
-	letter.style.display = 'block';
-	grid.style.display = 'inherit';
 	beginText.innerHTML = 'First letter of the 5 letter word you need to guess:';
 	randomWordId.innerHTML = randomWord.charAt(0).toUpperCase();
 	yourGuess.innerHTML = 'Your guess:';

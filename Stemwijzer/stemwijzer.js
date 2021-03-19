9b7c4f363541071ba1c36fdb1e8674bb695ae4c6
Unknown <99053654@mydavinci.nl>
2021-03-19 12:44:21 +0100

    added buttons for the pages with the questions and added a class for them

--- Stemwijzer/stemwijzer.js
+++ Stemwijzer/stemwijzer.js
@@ -3,7 +3,8 @@ const partijen = document.getElementById("partijen");
 const title = document.getElementById("title");
 const titleDescription = document.getElementById("title-description");
 const description = document.getElementById("description");
-const button = document.getElementById("button");
+const buttonStart = document.getElementById("button");
+var buttons = [];
 start();
 
 var sceneIndex = 0;
@@ -11,8 +12,8 @@ var sceneIndex = 0;
 function start() {
 	titleDescription.innerHTML = 'Tweede kamer verkiezingen 2021';
 	description.innerHTML = 'Test je politieke voorkeur aan de hand van 30 stellingen';
-	button.innerHTML = 'Start';
-	button.onclick = loadScene;
+	buttonStart.innerHTML = 'Start';
+	buttonStart.onclick = loadScene;
 }
 
 function choice() {
@@ -20,7 +21,12 @@ function choice() {
 }
 
 function loadScene() {
-	titleDescription.innerHTML = parties[sceneIndex].title;
-	description.innerHTML = parties[sceneIndex].statement;
-	
+	titleDescription.innerHTML = subjects[sceneIndex].title;
+	description.innerHTML = subjects[sceneIndex].statement;
+	stemwijzer.classList.add('scenePagina');
+	for (index = 0; index <=2; index++) {
+		var button = document.createElement("BUTTON");
+		buttons.push(button);
+		stemwijzer.appendChild(button);
+	}
 }
